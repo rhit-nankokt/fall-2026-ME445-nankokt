434ddfb01d7806843b74b7e6967d72ccee66c9ea
Kolton Nanko <nankokt@rose-hulman.edu>
2026-09-21 14:07:53 -0400

    initial pyserial learning

--- lab3/initial_pyserial_learning.py
+++ lab3/initial_pyserial_learning.py
@@ -2,8 +2,10 @@ import serial
 
 print("learning pyserial")
 
-ser = serial.Serial("COM5", 19200, timeout=10)
-ser.open()
+ser = serial.Serial("/dev/ttyACM0", 19200, timeout=10)
 while not ser.is_open:
     print("Port is open")
-    
+
+
+
+ser.close()
